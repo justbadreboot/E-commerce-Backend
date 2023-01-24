77f079a7e4624aa1f4f2e8f417a9e87d59431ad6
BryanArmando <bryanquisaguano@gmail.com>
2023-01-23 19:27:11 -0500

    creacion microservicio para landing page

---
+++ landing/src/main/java/com/microservice/landing/LandingApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.landing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandingApplication.class, args);
+	}
+
+}
