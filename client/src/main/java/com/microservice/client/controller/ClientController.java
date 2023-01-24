0129abce22e43f6e5e69586f64a5e463463855c0
Jonathan <jonato96s@gmail.com>
2023-01-23 22:42:15 -0500

    Adding config

--- client/src/main/java/com/microservice/client/controller/ClientController.java
+++ client/src/main/java/com/microservice/client/controller/ClientController.java
@@ -13,7 +13,6 @@ import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/client")
