e0fd3166bd3b778f95fd551d241f6d6cc34b7fcf
Jonathan <jonato96s@gmail.com>
2023-01-23 23:02:19 -0500

    Adding direction entity

---
+++ client/src/main/java/com/microservice/client/entity/Direction.java
@@ -0,0 +1,21 @@
+package com.microservice.client.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table
+public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String city;
+    private String state;
+    private String sector;
+    private String postalCode;
+    private String mainStreet;
+    private String secondStreet;
+    private String houseNumber;
+}
