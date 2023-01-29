682262cd8ad2c71982a565606db71d71263bcff9
Jonathan <jonato96s@gmail.com>
2023-01-28 20:56:02 -0500

    Added controllers methods

--- service/src/main/java/com/microservice/service/controller/ServiceController.java
+++ service/src/main/java/com/microservice/service/controller/ServiceController.java
@@ -1,4 +1,40 @@
 package com.microservice.service.controller;
 
+import com.microservice.service.entity.Service;
+import com.microservice.service.services.IService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/service")
 public class ServiceController {
+
+    @Autowired
+    private IService service;
+
+    @GetMapping
+    public List<Service> findAllServices(){
+        return service.findAll();
+    }
+    @GetMapping("/{id}")
+    public Service findServiceById(@PathVariable("id") Integer id){
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Service createService(Service srv){
+        return service.create(srv);
+    }
+
+    @PutMapping
+    public Service editService(Service srv){
+        return service.update(srv);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteService(@PathVariable("id") Integer id){
+        service.remove(id);
+    }
 }
--- service/src/main/java/com/microservice/service/controller/SpecialtyController.java
+++ service/src/main/java/com/microservice/service/controller/SpecialtyController.java
@@ -1,4 +1,43 @@
 package com.microservice.service.controller;
 
+import com.microservice.service.entity.Specialty;
+import com.microservice.service.services.SpecialtyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/specialty")
 public class SpecialtyController {
+    @Autowired
+    private SpecialtyService service;
+
+    @GetMapping
+    public List<Specialty> findAllSpecialties(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Specialty findSpecialtyById(@PathVariable("id") Integer id){
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Specialty createSpecialty(Specialty specialty){
+        return service.create(specialty);
+    }
+
+    @PutMapping
+    public Specialty editSpecialty(Specialty specialty){
+        return service.edit(specialty);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSpecialty(@PathVariable("id") Integer id){
+        service.remove(id);
+    }
+
+
+
 }
