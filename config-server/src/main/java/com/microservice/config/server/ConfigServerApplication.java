1dcd17aaaa68bbd983310bffcf7c4d897824563e
Jonathan <jonato96s@gmail.com>
2023-01-31 08:44:22 -0500

    Added config configuration

--- config-server/src/main/java/com/microservice/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/microservice/config/server/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.microservice.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
