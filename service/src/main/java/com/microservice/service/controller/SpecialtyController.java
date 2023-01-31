345cb775d57196cd949948661b1b9eede03f7bac
Jonathan <jonato96s@gmail.com>
2023-01-31 14:10:12 -0500

    Added cross origin

--- service/src/main/java/com/microservice/service/controller/SpecialtyController.java
+++ service/src/main/java/com/microservice/service/controller/SpecialtyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/specialty")
 public class SpecialtyController {
     @Autowired
