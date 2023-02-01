d780ebb06bdb3979f8b8e8720e87645aaf03ee61
BryanArmando <bryanquisaguano@gmail.com>
2023-02-01 00:56:33 -0500

    habilidar Eureka client, dependencias, configuraciones y anotaciones

--- landing/src/main/java/com/microservice/landing/LandingApplication.java
+++ landing/src/main/java/com/microservice/landing/LandingApplication.java
@@ -2,7 +2,9 @@ package com.microservice.landing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LandingApplication {
 
