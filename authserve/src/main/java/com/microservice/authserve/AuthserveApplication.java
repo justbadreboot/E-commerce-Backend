7c1dc7a6892457eaadf4cfa24ed267c5752e7fba
BryanArmando <bryanquisaguano@gmail.com>
2023-01-31 20:09:19 -0500

    Lombok MapStruct

---
+++ authserve/src/main/java/com/microservice/authserve/AuthserveApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.authserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserveApplication.class, args);
+	}
+
+}
