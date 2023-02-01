9c794751c632535972aa12239ba2db6ab231eb1f
Jonathan <jonato96s@gmail.com>
2023-01-31 22:53:02 -0500

    Rename route to get ping

--- config-server/src/main/java/com/microservice/config/server/controller/StatusController.java
+++ config-server/src/main/java/com/microservice/config/server/controller/StatusController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/config")
+@RequestMapping("/api/ping")
 public class StatusController {
     @GetMapping
     public String state(){
-        return "Actually Works";
+        return "Config Server Actually Works Fine";
     }
 
 }
