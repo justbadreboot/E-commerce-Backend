aaa7cffc15ab3bceb38261c69fd01a2bd42b8161
Jonathan <jonato96s@gmail.com>
2023-02-01 15:22:49 -0500

    Added mapstruct and DTO

---
+++ client/src/main/java/com/microservice/client/dto/ClientDTO.java
@@ -0,0 +1,26 @@
+package com.microservice.client.dto;
+
+import com.microservice.client.entity.Direction;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ClientDTO {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String document;
+    private String phone;
+    private List<Direction> directions = new ArrayList<>();
+
+}
