e01842719e6c5bb4c00661161f91dc75645659f0
Jonathan <jonato96s@gmail.com>
2023-02-02 14:49:12 -0500

    Added doctor dto

---
+++ service/src/main/java/com/microservice/service/dto/DoctorGetDTO.java
@@ -0,0 +1,10 @@
+package com.microservice.service.dto;
+
+public class DoctorGetDTO {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private Integer specialtyId;
+}
---
+++ service/src/main/java/com/microservice/service/dto/DoctorPostDTO.java
@@ -0,0 +1,11 @@
+package com.microservice.service.dto;
+import com.microservice.service.entity.Specialty;
+
+public class DoctorPostDTO {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private Integer specialtyId;
+}
