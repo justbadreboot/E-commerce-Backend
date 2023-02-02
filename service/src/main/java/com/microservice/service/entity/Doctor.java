4e6823c6c3c20698718dfda898c0c0d6ec07fd2e
Jonathan <jonato96s@gmail.com>
2023-02-02 11:23:15 -0500

    Added realtionships between entities

--- service/src/main/java/com/microservice/service/entity/Doctor.java
+++ service/src/main/java/com/microservice/service/entity/Doctor.java
@@ -13,7 +13,6 @@ public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String name;
     private String phone;
     private String email;
--- service/src/main/java/com/microservice/service/entity/Specialty.java
+++ service/src/main/java/com/microservice/service/entity/Specialty.java
@@ -3,6 +3,9 @@ package com.microservice.service.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -11,5 +14,6 @@ public class Specialty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotBlank
     private String name;
 }
