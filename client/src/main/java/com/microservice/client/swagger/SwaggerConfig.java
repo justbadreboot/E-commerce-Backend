145cd9e1c9583bf68ed7126219e5baec769abdc3
Jonathan <jonato96s@gmail.com>
2023-02-05 18:44:12 -0500

    Remove actuator couse of conflicts

--- client/src/main/java/com/microservice/client/swagger/SwaggerConfig.java
+++ client/src/main/java/com/microservice/client/swagger/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.microservice.client.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
