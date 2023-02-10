85b92c8849b0892f2b37677f9710d1f68997c83c
BryanArmando <bryanquisaguano@gmail.com>
2023-02-09 20:53:17 -0500

    eliminar loadbalancer de gateway

--- api-gateway/src/main/java/com/microservice/api/gateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/config/WebClientConfig.java
@@ -1,6 +1,5 @@
 package com.microservice.api.gateway.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,7 +8,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
     public WebClient.Builder builder(){
         return WebClient.builder();
     }
