b877798f56f9f4270b3fda80234cc58f0a325d9f
BryanArmando <bryanquisaguano@gmail.com>
2023-02-09 19:22:20 -0500

    implementacion de bean component para uso eficiente

--- api-gateway/src/main/java/com/microservice/api/gateway/config/AuthFilter.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/config/AuthFilter.java
@@ -7,10 +7,12 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
     private WebClient.Builder webClient;
