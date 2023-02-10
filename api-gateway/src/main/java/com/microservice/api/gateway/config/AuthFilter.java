61b6da13e98b961ab9e4258c8432f0e4fb75147a
BryanArmando <bryanquisaguano@gmail.com>
2023-02-09 19:05:43 -0500

    conexion gateway con auth server para seguridad entre microservicios

---
+++ api-gateway/src/main/java/com/microservice/api/gateway/config/AuthFilter.java
@@ -0,0 +1,54 @@
+package com.microservice.api.gateway.config;
+
+import com.microservice.api.gateway.dto.RequestDTO;
+import com.microservice.api.gateway.dto.TokenDTO;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+
+    private WebClient.Builder webClient;
+
+    public AuthFilter(WebClient.Builder webClient){
+        super(Config.class);
+        this.webClient=webClient;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (((exchange, chain) -> {
+            if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            }
+            String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String [] chunks = tokenHeader.split(" ");
+            if (chunks.length !=2 || !chunks[0].equals("Bearer")){
+                return onError(exchange, HttpStatus.BAD_REQUEST);
+            }
+            return webClient.build()
+                    .post()
+                    .uri("https://authserve-production.up.railway.app/auth/validate?token="+ chunks[1])
+                    .bodyValue(new RequestDTO(exchange.getRequest().getPath().toString(), exchange.getRequest().getMethod().toString()))
+                    .retrieve().bodyToMono(TokenDTO.class)
+                    .map(t -> {
+                        t.getToken();
+                        return exchange;
+                    }).flatMap(chain::filter);
+        }));
+    }
+
+    public Mono<Void> onError(ServerWebExchange exchange, HttpStatus status){
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
+    }
+
+    public static class Config{}
+
+}
---
+++ api-gateway/src/main/java/com/microservice/api/gateway/dto/RequestDTO.java
@@ -0,0 +1,17 @@
+package com.microservice.api.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class RequestDTO {
+
+    private String uri;
+
+    private String method;
+}
