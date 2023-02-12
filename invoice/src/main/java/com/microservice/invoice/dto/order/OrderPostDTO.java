185809e788dda3be00e85ab07e167a5030d4bcc9
Jonathan <jonato96s@gmail.com>
2023-02-12 18:23:44 -0500

    Added dto validation

--- invoice/src/main/java/com/microservice/invoice/dto/order/OrderPostDTO.java
+++ invoice/src/main/java/com/microservice/invoice/dto/order/OrderPostDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
@@ -42,6 +43,7 @@ public class OrderPostDTO {
     @NotNull
     private StatePostDTO deliveryState;
     @NotNull
+    @NotEmpty
     private List<OrderDetailPostDTO> orderDetails;
 
 }
