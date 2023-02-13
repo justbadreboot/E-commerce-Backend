2881526466805c7e7b012246ed2afecb8ce0d089
BryanArmando <bryanquisaguano@gmail.com>
2023-02-13 13:24:22 -0500

    enable global cors

--- api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
@@ -3,9 +3,6 @@ package com.microservice.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,19 +11,5 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*")
-				 .allowedOrigins("*")
-				 .allowedMethods("GET", "POST","PUT","DELETE").allowedHeaders("*")
-				 .maxAge(3600);
-				 }
-
-				 };
-				 }
 
 }
