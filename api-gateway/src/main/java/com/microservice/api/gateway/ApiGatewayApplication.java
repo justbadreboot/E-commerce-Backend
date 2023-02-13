0cfaea6caa39c85c901ff93cba3af877d9ebece7
BryanArmando <bryanquisaguano@gmail.com>
2023-02-13 17:12:03 -0500

    cors ea

--- api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
@@ -3,7 +3,9 @@ package com.microservice.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
