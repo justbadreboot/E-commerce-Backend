f9c964d8746a70fb2fe70a4e3d9655fa272bd505
BryanArmando <bryanquisaguano@gmail.com>
2023-02-13 12:42:48 -0500

    cors

--- api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.microservice.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,19 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*")
+				 .allowedOrigins("*")
+				 .allowedMethods("GET", "POST","PUT","DELETE").allowedHeaders("*")
+				 .maxAge(3600);
+				 }
+
+				 };
+				 }
 
 }
