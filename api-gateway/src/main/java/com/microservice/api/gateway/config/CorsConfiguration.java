4b9d5f72632e67ab52fd22f04e124b1f72b5fd27
BryanArmando <bryanquisaguano@gmail.com>
2023-02-14 15:39:53 -0500

    new resource cors

--- api-gateway/src/main/java/com/microservice/api/gateway/config/CorsConfiguration.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/config/CorsConfiguration.java
@@ -2,9 +2,8 @@ package com.microservice.api.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -16,15 +15,17 @@ public class CorsConfiguration extends org.springframework.web.cors.CorsConfigur
         org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedOrigin("http://localhost:3000/");
+        corsConfiguration.addAllowedOrigin("https://product-production-cf12.up.railway.app/");
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         corsConfiguration.addAllowedHeader("origin");
         corsConfiguration.addAllowedHeader("content-type");
         corsConfiguration.addAllowedHeader("accept");
-        corsConfiguration.addAllowedHeader("authorization");
+        corsConfiguration.addAllowedHeader("Authorization");
         corsConfiguration.addAllowedHeader("cookie");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter((CorsConfigurationSource) source);
+        return new CorsWebFilter(source);
     }
 
 }
