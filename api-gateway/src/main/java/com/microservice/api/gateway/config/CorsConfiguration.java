6e38db28aa3dcf5f61b660eb50a1352e5341aa8c
BryanArmando <bryanquisaguano@gmail.com>
2023-02-14 16:22:10 -0500

    * header

--- api-gateway/src/main/java/com/microservice/api/gateway/config/CorsConfiguration.java
+++ api-gateway/src/main/java/com/microservice/api/gateway/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration extends org.springframework.web.cors.CorsConfigur
     public CorsWebFilter corsFilter() {
         org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.applyPermitDefaultValues();
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         corsConfiguration.addAllowedHeader("origin");
