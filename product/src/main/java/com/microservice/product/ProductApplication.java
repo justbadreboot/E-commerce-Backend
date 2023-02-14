3e3ce6382b9709efbc802580f578d8335cb431bb
BryanArmando <bryanquisaguano@gmail.com>
2023-02-13 20:16:30 -0500

    ** cors app unic

--- product/src/main/java/com/microservice/product/ProductApplication.java
+++ product/src/main/java/com/microservice/product/ProductApplication.java
@@ -3,9 +3,12 @@ package com.microservice.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin(origins = "**", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE, RequestMethod.PUT})
 public class ProductApplication {
 
     public static void main(String[] args) {
