24cd36b861d530b2c24e7fe3ec03de9863748658
Jonathan <jonato96s@gmail.com>
2023-02-16 20:22:41 -0500

    Added resilience4j circiut breaker

--- client/src/test/java/com/microservice/client/controller/query/ClientQueryControllerTest.java
+++ client/src/test/java/com/microservice/client/controller/query/ClientQueryControllerTest.java
@@ -10,10 +10,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -55,7 +53,4 @@ public class ClientQueryControllerTest {
         assertEquals(client, result.getBody());
         Mockito.verify(clientService).findById(1);
     }
-
-
-
 }
